b9d4172ade12e03774891b2b99f19e8e2b42262d
Niklas Schulze <me@jns.io>
2022-01-27 16:37:40 +0100

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -25,72 +25,18 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    final p = PlayerPlatform.instance;
+    final player = await PlayerPlatform.instance
+        .createPlayer(environmentArgs: ['--no-osd']);
+    _player = player;
 
-    //final envId = await p.createEnvironment(args: ['ABC', 'DEF']);
-    //final player = await p.createPlayer(environmentId: envId);
+    if (mounted) {
+      setState(() {});
+    }
 
-    _player = await p.createPlayer(environmentArgs: [
-      '--no-osd',
-      '--file-caching=10000',
-      '--high-priority',
-      '--no-audio'
-    ]);
-    final player = _player!;
-
-    setState(() {});
-
-    player.generalStream.listen((event) {
-      print("VOLUME CHANGED ${event.volume}");
-    });
-
-    player.playbackStream.listen((pb) {
-      print("Status ${pb.playbackState}");
-    });
-
-    player.currentMediaStream.listen((event) {
-      int x = 5;
-    });
-
-    //await player?.dispose();
-
-    final m = Media.file(File(r'C:\Users\10261369\Videos\a.mp4'));
-
-    //await p.disposeEnvironment(envId);
-
-    final m2 = Media.file(File(r'C:\Users\10261369\Videos\b.mp4'));
-
-    //await player?.open(m);
-
-    final pl = Playlist(medias: [m, m2], playlistMode: PlaylistMode.loop);
-
+    final m = Media.file(File(r'some_video.mp4'));
     await player.open(m);
     await player.setPlaylistMode(PlaylistMode.loop);
-
-    // await player.setVolume(0.75);
-
-    //await player.seekTime(Duration(seconds: 5));
-
-    //await player.play();
-
-    await Future.delayed(Duration(seconds: 15));
-    //await player.stop();
-
-    await player.open(m2);
-
-    //await Future.delayed(Duration(seconds: 5));
-
-    //await player.dispose();
-
-    //await player.play();
-
-    //await player?.stop();
-
-    //await player?.dispose();
-
-    //int x = 5;
   }
 
   @override
